fc1b28d2612c85e7037c2645fafe1c806bd6f69f
w84scaler <saint.neki@gmail.com>
2021-05-26 18:37:26 +0300

    4 lab

--- server/controllers/taskcontroller.js
+++ server/controllers/taskcontroller.js
@@ -1,69 +1,83 @@
-const Task = require('../models/Task');
+const QUERY = require("../includes/queries");
+const fs = require('fs');
 
-exports.add = async(data) => {
-    let files = [];
-    data.files.forEach(file => {
-        files.push(file.filename);
+exports.getTasks = function(socket){
+    socket.on("GET_TASKS", function(status, callback) {
+        QUERY.getTasks(status)
+            .then(data => {
+                callback({statusCode: 200, result:data});
+            })
+            .catch(error => {
+                console.log(error);
+                callback({ statusCode: 400, msg: "Bad request"})
+            })
     })
-    const task = new Task({
-        name: data.body.name,
-        desc: data.body.desc,
-        start_date: data.body.start_date,
-        end_date: data.body.end_date,
-        status: data.body.status,
-        files: files
-    });
-
-    try {
-        const savedTask = await task.save();
-        return savedTask;
-    }
-    catch(err) {
-        return null;
-    }
 }
 
-exports.update = async(data) => {
-    let files = [];
-    data.files.forEach(file => {
-        files.push(file.filename);
+exports.deleteTask = function(socket){
+    socket.on("DELETE_TASK", (id, callback, sm) => {
+        console.log(id);
+        console.log(callback)
+        QUERY.deleteTask(id)
+            .then(code => {
+                callback({statusCode: code});
+            })
+            .catch(err => {
+                console.log(err);
+                callback({statusCode: 404});
+            })
     })
-    try {
-        data.body.files = files;
-        const task = await Task.findByIdAndUpdate({_id : data.body.id}, data.body, {upsert: false})
-        return task;
-    }
-    catch(err) {
-        return null;
-    }
 }
 
-exports.list = async() => {
-    try {
-        const tasks = await Task.find();
-        return tasks;
-    }
-    catch(err) {
-        return null;
-    }
+exports.downloadFile= function(socket){
+    socket.on("DOWNLOAD_FILE", (fileName, callback) => {
+        fs.readFile('./server/files/' + fileName, (err, buff) => {
+            if(!err){
+                callback({statusCode: 200, result: buff});
+            } else {
+                callback({ statusCode: 500, msg: 'Enternal server error'})
+            }
+        });
+    })
 }
 
-exports.listFilterStatus = async(status) => {
-    try {
-        const tasks = await Task.find({status: status});
-        return tasks;
-    }
-    catch(err) {
-        return null;
-    }
+exports.createTask = async function(socket){
+    socket.on("CREATE_TASK", (data, callback) => {
+        loadFile(data);
+        QUERY.insertTask(data)
+        .then(code => {
+            callback({statusCode: 201});
+        })
+        .catch(err => {
+            console.log(err);
+            callback({statusCode: 500});
+        });
+    }) 
 }
 
-exports.delete = async(id) => {
-    try {
-        const task = await Task.findByIdAndRemove({_id: id});
-        return true;
-    }
-    catch(err) {
-        return null;
+exports.updateTask = async function(socket){
+    socket.on("UPDATE_TASK", (data, callback) => {
+        loadFile(data);
+        QUERY.updateTask(data)
+            .then(() => {
+                callback({ statusCode : 204 });
+            })
+            .catch(() => {
+                callback({ statusCode : 400});
+            })
+        
+    })
+}
+
+function loadFile(data){
+    if(data.binfiles){
+        data.dbfilenames = [];
+        for (let i = 0; i < data.binfiles.length; i++){
+            data.dbfilenames[i] = Date.now() + '-' + data.filenames[i];
+            fs.writeFile('./server/files/' + data.dbfilenames[i] , data.binfiles[i], err => {
+                if(err)
+                    console.log(err);
+            })
+        }
     }
 }
